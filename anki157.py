f5896a82034b9f7c53775944a4d33e91290f4dd8
16nikki <gpta157.nikhilgowda.s.l@gmail.com>
2025-09-03 14:57:53 +0530

    Add files via upload

---
+++ anki157.py
@@ -0,0 +1,48 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "fd47d9cc",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def add (a,b):\n",
+    "    return a+b\n",
+    "def sub (a,b):\n",
+    "     return a-b"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8aeec7f0",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "impor"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
